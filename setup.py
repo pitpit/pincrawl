d5bb69173e5be299f83f95da50c3c2cf41929076
pitpit <damien.pitard@gmail.com>
2025-09-16 22:13:53 +0200

    fix package structure

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
-            'pincrawl=cli:main',
+            'pincrawl=pincrawl.cli:main',
         ],
     },
     include_package_data=True,
